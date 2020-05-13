7325bb26b10f1d089a1288cfed1cdd62ff5e8940
Andrew Ebert <andrewsebertii@gmail.com>
2020-05-13 15:31:00 -0500

    sliderStillTest

---
+++ swiper.js
@@ -0,0 +1,11 @@
+// // import Swiper from "swiper";
+// window.onload = function () {
+//     //initialize swiper when document ready
+//     var mySwiper = new Swiper ('.swiper-container', {
+//       // Optional parameters
+//      // direction: 'horizontal',
+//      //  loop: true
+//     })
+//   };
+
+  
